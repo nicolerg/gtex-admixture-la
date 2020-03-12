bcaf2b38a29f59e798f20fbb0709137f065554a6
nicolerg <nicolerg@stanford.edu>
2020-03-12 12:56:20 -0700

    add rfmix

--- ALL/rfmix_to_ancestry_bed/deprecated/fa-to-dict.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Make .dict files for hg38 chromosomes
-for n in {1..22}
-do 
-	input="/mnt/lab_data/montgomery/nicolerg/hg38-ref/chr${n}.fa"
-	output="/mnt/lab_data/montgomery/nicolerg/hg38-ref/chr${n}.dict"
-	java -jar picard.jar CreateSequenceDictionary R=${input} O=${output}
-done
